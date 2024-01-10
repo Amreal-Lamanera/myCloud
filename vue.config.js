ea156392fe1d01c2efbe3500a1e3041683d02e68
Francesco Pieraccini <pieraccini.francesco@gmail.com>
2024-01-10 03:14:09 +0100

    edit dir name

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   // TODO:
   // LOCALE
-  // publicPath: process.env.NODE_ENV === 'production' ? '/admin/c-panel/iPieraCloud/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/admin/c-panel/myCloud/' : '/',
   // ONLINE
-  publicPath: process.env.NODE_ENV === 'production' ? '/my_site/admin/c-panel/iPieraCloud/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/my_site/admin/c-panel/myCloud/' : '/',
 })

233a75d0c6a2ecbcafa34867d4b88e493a436bfe
Francesco Pieraccini <francesco.pieraccini@vola.it>
2024-01-10 16:17:50 +0100

    sistemate icone

--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,55 @@ module.exports = defineConfig({
   // publicPath: process.env.NODE_ENV === 'production' ? '/admin/c-panel/myCloud/' : '/',
   // ONLINE
   publicPath: process.env.NODE_ENV === 'production' ? '/my_site/admin/c-panel/myCloud/' : '/',
+  pwa: {
+    name: 'myCloud',
+    themeColor: '#0f172a',
+    msTileColor: '#0f172a',
+    msTileImage: '/my_site/src/img/loghi/favicon/apple-touch-icon.png',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    display_override: [
+      "fullscreen",
+      "minimal-ui",
+      "window-controls-overlay"
+    ],
+    display: "standalone",
+    iconPaths:
+        {
+          faviconSVG: '../../../src/img/loghi/favicon/favicon.svg',
+          favicon32: '../../../src/img/loghi/favicon/favicon-32x32.png',
+          favicon16: '../../../src/img/loghi/favicon/favicon-16x16.png',
+          appleTouchIcon: '../../../src/img/loghi/favicon/apple-touch-icon.png',
+          maskIcon: '../../../src/img/loghi/favicon/favicon.svg',
+          msTileImage: '../../../src/img/loghi/favicon/apple-touch-icon.png'
+        },
+    manifestOptions: {
+      icons: [
+        {
+          src: '/my_site/src/img/loghi/favicon/apple-touch-icon.png',
+          sizes: '180x180',
+          type: 'image/png',
+        },
+        {
+          src: '/my_site/src/img/loghi/favicon/favicon.svg',
+          type: 'image/svg+xml',
+        },
+        {
+          src: '/my_site/src/img/loghi/favicon/favicon-16x16.png',
+          sizes: '16x16',
+          type: 'image/png',
+        },
+        {
+          src: '/my_site/src/img/loghi/favicon/favicon-32x32.png',
+          sizes: '32x32',
+          type: 'image/png',
+        },
+        {
+          src: '/my_site/src/img/loghi/favicon/favicon-48x48.png',
+          sizes: '48x48',
+          type: 'image/png',
+        },
+      ],
+    },
+  },
 })
